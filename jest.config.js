e68f7ccf1149f753e2e350e7b30e2328b790e2e8
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-01-09 09:17:48 +0100

    chore: integrate swagger-ui with React@18 support

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   rootDir: __dirname,
-  testEnvironment: 'jest-environment-jsdom',
+  // testEnvironment: 'jest-environment-jsdom',
   testMatch: [
     '**/test/unit/**/*.js',
   ],
@@ -11,6 +11,6 @@ module.exports = {
     "/node_modules/(?!(swagger-ui-4|swagger-ui-react-4|swagger-ui-5|swagger-ui-react-5|swagger-client|react-syntax-highlighter)/)"
   ],
   setupFiles: ['<rootDir>/test/jest-shim.js'],
-  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  // setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   silent: false,
 };
